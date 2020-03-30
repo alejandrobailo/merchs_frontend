6c2c82ce832396b32c135c433bf23fc955126fb0
Antxon Eguiguren <antxonegi@gmail.com>
2020-03-30 18:52:20 +0200

    Chorradas

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { logout } from '../utils'
+import { logout } from '../utils';
 import { Router } from '@angular/router';
 
 @Component({
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,8 +1,8 @@
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 
 export function logout(router: Router) {
     localStorage.removeItem('token');
     localStorage.removeItem('customerIdKanala');
-    localStorage.removeItem('cart')
+    localStorage.removeItem('cart');
     router.navigate(['/']);
 }
